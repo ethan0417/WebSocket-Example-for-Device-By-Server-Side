fffca0456da0b457a32dc27b6c58ac53275ec02c
Ethan <ethan.mail0417@gmail.com>
2014-01-02 17:44:18 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 WebSocket-Example-for-Device-By-Server-Side
 ===========================================
+Client-side : [WebSocket-Example-for-Device-By-Chrome-Extension](https://github.com/ethan0417/WebSocket-Example-for-Device-By-Chrome-Extension) 
+
+This example is a copy of example from
+[websocket-multiplex](https://github.com/sockjs/websocket-multiplex/)
+
+project:
+
+ * https://github.com/sockjs/websocket-multiplex/
+
+
+To run this example, first install dependencies:
+
+    npm install
+
+And run a server:
+
+    node server.js
+
+
+That will spawn an http server at http://127.0.0.1:9999/ which will
+serve both html (served from the current directory) and also SockJS
+service (under the [/multiplex](http://127.0.0.1:9999/multiplex)
+path).
+
+With that set up, WebSocket-multiplex is able to push three virtual
+connections over a single SockJS connection. See the code for details.
---
+++ server.js
@@ -0,0 +1,51 @@
+var express             = require('express');
+var sockjs              = require('sockjs');
+
+var websocket_multiplex = require('websocket-multiplex');
+
+// 1. Setup SockJS server
+var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
+var service = sockjs.createServer(sockjs_opts);
+
+
+// 2. Setup multiplexing
+var multiplexer = new websocket_multiplex.MultiplexServer(service);
+
+
+// 3. Express server
+var app = express.createServer();
+service.installHandlers(app, {prefix:'/multiplex'});
+
+console.log(' [*] Listening on 0.0.0.0:9999' );
+app.listen(9999, '0.0.0.0');
+
+app.get('/:id/client', function (req, res) {
+    res.sendfile(__dirname + '/1.html');
+});
+
+app.get('/:id', function(req, res){
+  var ann = multiplexer.registerChannel(req.params.id);
+  var connect = [];
+  ann.on('connection', function(conn) {
+      connect.push(conn);
+      
+      //conn.write('Ann says hi!');
+      conn.on('data', function(data) {
+        console.log('==== data ====');
+        console.log(data);
+        connect[0].write(data);
+        /*
+        for(var i=0; i<connect.length; i++){
+          connect[i].write(data);
+        }
+        */
+        //conn.write(data);
+      });
+  });
+  //res.sendfile(__dirname + '/1.html');
+  res.send({status:true});
+});
+
+app.get('/multiplex.js', function (req, res) {
+    res.sendfile(__dirname + '/multiplex.js');
+});
